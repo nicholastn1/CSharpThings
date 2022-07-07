122032574691097eddeaf2b84b1df85ab983a644
Nicholas Timbó Nogueira <nicholas.nogueira@fitbank.com.br>
2022-07-07 14:29:23 -0300

    Adding my first MVC application to the repo

---
+++ MeuSiteEmMVC/MeuSiteEmMVC/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using MeuSiteEmMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuSiteEmMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Nicholas Timbó";
+            home.Email = "nicholasnogueira12@gmail.com";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MyApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization.CultureInfo;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            // Show the Fibonacci sequence
+            Console.WriteLine("Fibonacci sequence:");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(Fibonacci(i));
+            }
+
+            // Show the factorial of a number
+            Console.WriteLine("Factorial of 5:");
+            Console.WriteLine(Factorial(5));
+
+        }
+    }
+}
